938f99295dda6477daf10d844cd85237e5b77858
Tom Duncalf <tom@tomduncalf.com>
2021-03-27 21:20:33 +0000

    Regenerate dist

--- LiveFader/dist/maxForLiveUtils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.getLiveObjectById = void 0;
-var getLiveObjectById = function (id) {
-    var api = new LiveAPI();
-    api.id = Number(id);
-    return api;
-};
-exports.getLiveObjectById = getLiveObjectById;
